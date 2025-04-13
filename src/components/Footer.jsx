893c92d85f07b07ac103638ee30c52ac98e48789
Mehdi Dinari <mehdi.dinari@usmba.ac.ma>
2025-04-13 11:41:55 +0100

    foot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
                         <svg className="footer-icon" viewBox="0 0 20 20">
                             <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
                         </svg>
-                        <span>123 Rue de l'École, Ville</span>
+                        <span>123 Rue de l'École,Ville</span>
                     </div>
                 </div>
 
